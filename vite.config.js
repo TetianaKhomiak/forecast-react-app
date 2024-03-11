ba0f300a6c90d8296ef3d23025e4ec14b7f3a9c3
TetianaKhomiak <tkhomiak8889@gmail.com>
2024-03-11 20:48:26 +0100

    add .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/forecast-react-app/" : "/",
+  base: "/forecast-react-app/",
   plugins: [react()],
 });
